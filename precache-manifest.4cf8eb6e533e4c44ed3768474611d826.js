ca9320d879083d547082f52623024a962d2119be
loaded02 <ocaptainmycaptain@web.de>
2020-02-09 00:00:12 +0100

    Updates

---
+++ precache-manifest.4cf8eb6e533e4c44ed3768474611d826.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c87ccb452477fe71bf436222f956161e",
+    "url": "/ICF_Report_Webapp_React/index.html"
+  },
+  {
+    "revision": "2f24026e5df3bade6536",
+    "url": "/ICF_Report_Webapp_React/static/css/2.ee3fb633.chunk.css"
+  },
+  {
+    "revision": "6503da582b063e6fa183",
+    "url": "/ICF_Report_Webapp_React/static/css/main.b3d9b13f.chunk.css"
+  },
+  {
+    "revision": "2f24026e5df3bade6536",
+    "url": "/ICF_Report_Webapp_React/static/js/2.fffc939e.chunk.js"
+  },
+  {
+    "revision": "a0bc000d2867046799fc541f7155859a",
+    "url": "/ICF_Report_Webapp_React/static/js/2.fffc939e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6503da582b063e6fa183",
+    "url": "/ICF_Report_Webapp_React/static/js/main.2f55c8f6.chunk.js"
+  },
+  {
+    "revision": "e47464677325564d682d",
+    "url": "/ICF_Report_Webapp_React/static/js/runtime-main.eb7a82bd.js"
+  },
+  {
+    "revision": "910a6457ce29b802f67110322df97952",
+    "url": "/ICF_Report_Webapp_React/static/media/background.910a6457.jpg"
+  },
+  {
+    "revision": "a770b6797b68e3f8920e473eb824bac0",
+    "url": "/ICF_Report_Webapp_React/static/media/loader-big.a770b679.gif"
+  },
+  {
+    "revision": "f6ef32c7bc5ec75433b786035a9df0fa",
+    "url": "/ICF_Report_Webapp_React/static/media/logo.f6ef32c7.svg"
+  },
+  {
+    "revision": "12f0820c451bdc75f4d1ef97732bf6e8",
+    "url": "/ICF_Report_Webapp_React/static/media/rw-widgets.12f0820c.woff"
+  },
+  {
+    "revision": "792dcd18baf5f544aabcad1883d673c2",
+    "url": "/ICF_Report_Webapp_React/static/media/rw-widgets.792dcd18.svg"
+  },
+  {
+    "revision": "bc7c4a59f924cf037aad6e1f9edba366",
+    "url": "/ICF_Report_Webapp_React/static/media/rw-widgets.bc7c4a59.eot"
+  },
+  {
+    "revision": "eceddf474df95d8d4a7e316668c3be85",
+    "url": "/ICF_Report_Webapp_React/static/media/rw-widgets.eceddf47.ttf"
+  }
+]);
